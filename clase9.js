872267effcc033362ab319f7f0758fab01aba383
luis banda <rockband_666@hotmail.com>
2021-07-04 22:13:54 -0400

    comparaciones en javaScript

--- clase9.js
+++ clase9.js
@@ -1,5 +1,7 @@
 var x = 4 , y = '4'
 //aca va dar false
+
+//objeto literal asi se llama 
 var luis = {
     nombre: 'luis'
 }
@@ -7,6 +9,8 @@ var luis = {
 var otraPersona = {
     nombre: 'luis'
 }
+//en la consola del navegador se  coloca el nombre directo para que reconosca y luego se compara 
+//cuando se comprara objetos no son lo mismo aunque sea con igual igual igual ya que esta guardado en otro espacio de memoria 
 
 //para que de verdadero seria de esta manera 
 
