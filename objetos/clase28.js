ddef2a93bad36407a6f9198d89758286805cc5fc
luis banda <rockband_666@hotmail.com>
2021-07-13 12:29:51 -0400

    aca se crea una lfuncion con un id para crear un url y asi pueda llamar varios nombres

--- objetos/clase28.js
+++ objetos/clase28.js
@@ -1,9 +1,12 @@
 const API_URL = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 
-    $.get(`${API_URL}${PEOPLE_URL.replace(':id',1)}` , { crossDomain: true},function (luke) {
-        console.log(`Hola yo soy, ${luke.name}`)
-    })
+//const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`
+const opts = { crossDomain: true}
+
+const onPeopleResponse = function (luke) {
+    console.log(`Hola yo soy, ${luke.name}`)
+}
 
 //colvag se puede llamar en un futuro 
 // Los que desean, les dejo algunos links para que usen otras APIS.
@@ -12,3 +15,10 @@ const PEOPLE_URL = 'people/:id'
 // Pokemon: https://pokeapi.co/
 // StarWars: https://swapi.dev/
 // Harry Potter: https://www.potterapi.com/
+
+
+// ahora haremos un llamdo de varios nombres 
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+    $.get( url , opts , onPeopleResponse)
+}
